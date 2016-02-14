48d99f1d52de8e8bf96d55ad4a78f30ac7715789
Lisa Chalaguine <lisa.chalaguine15@imperial.ac.uk>
2016-02-14 16:36:33 +0000

    d

--- src/simulation/TwoParticleCollision.java
+++ src/simulation/TwoParticleCollision.java
@@ -6,13 +6,19 @@ public class TwoParticleCollision extends Collision {
 	Particle particleOne;
 	Particle particleTwo;
 	
-	public TwoParticleCollision(Particle p1, Particle p2, double t) {            //should take two particles and time as arguments
-    /*NEED TO DO THIS*/
+	public TwoParticleCollision(Particle p1, Particle p2, double t)
+        {   
+        super(t, new Particle[] {p1, p2});  //not sure about that
+        this.time = t;
+	this.particleOne = p1;
+	this.particleTwo = p2;  
 	}
 
 	@Override
-	public void happen(ParticleEventHandler h) {
-	/*NEED TO DO THIS*/
+	public void happen(ParticleEventHandler h) 
+	{
+	Particle.collide(particleOne, particleTwo);
+	h.reactTo(this);	
 	}
 	
 }
